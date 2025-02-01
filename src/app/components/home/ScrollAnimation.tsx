cea986f6efc96434ce8acfdb1b0ced7497dbf353
gopakumar-k-a <gopak9145@gmail.com>
2025-02-01 18:24:18 +0530

    adde framer motion animation

--- src/app/components/home/ScrollAnimation.tsx
+++ src/app/components/home/ScrollAnimation.tsx
@@ -2,13 +2,17 @@ import { useRef, useState, useEffect } from "react";
 import { CaseStudyLink } from "../tag/CaseStudy";
 import { GlowTagGroup } from "../tag/glow-tag-group";
 import Image from "next/image";
-
+import { motion, AnimatePresence } from "framer-motion";
 import { datas } from "./datas";
 
 const ScrollAnimation = () => {
-  const [inViewArray, setInViewArray] = useState<boolean[]>(new Array(datas.length).fill(false));
+  const [inViewArray, setInViewArray] = useState<boolean[]>(
+    new Array(datas.length).fill(false)
+  );
 
-  const refs = useRef<(HTMLDivElement | null)[]>(new Array(datas.length).fill(null));
+  const refs = useRef<(HTMLDivElement | null)[]>(
+    new Array(datas.length).fill(null)
+  );
 
   const handleVisibilityChange = (index: number, isInView: boolean) => {
     const updatedArray = [...inViewArray];
@@ -22,7 +26,7 @@ const ScrollAnimation = () => {
         return datas[i].imgLink;
       }
     }
-    return datas[0].imgLink; 
+    return datas[0].imgLink;
   };
 
   useEffect(() => {
@@ -48,30 +52,43 @@ const ScrollAnimation = () => {
   return (
     <div className="bg-black text-white mx-auto max-w-screen-xl">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="sticky top-32 h-[60vh] flex">
-          <Image
-            src={getCurrentImage()}
-            alt="Project Preview"
-            width={800} 
-            height={600}
-            className="w-full h-full object-cover transition-all duration-500 ease-in-out"
-          />
-        </div>
+        <AnimatePresence>
+          <motion.div
+            key={getCurrentImage()}
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="sticky top-32 h-[60vh] flex"
+          >
+            <Image
+              src={getCurrentImage()}
+              alt="Project Preview"
+              width={800}
+              height={600}
+              className="w-full h-full object-cover transition-all duration-500 ease-in-out"
+            />
+          </motion.div>
+        </AnimatePresence>
 
         <div>
           {datas.map((data, index) => {
             return (
-              <div key={index} 
-              ref={(el) => {
-                refs.current[index] = el;
-              }}
-              className="py-24">
-
+              <div
+                key={index}
+                ref={(el) => {
+                  refs.current[index] = el;
+                }}
+                className="py-24"
+              >
                 <div className="pb-6">
                   <GlowTagGroup tags={data.tags} />
                 </div>
-                <h1 className="font-normal font-tggMedium text-5xl">{data.heading}</h1>
-                <p className="text-gray-400 text-sm font-semibold font-tggBold mt-4">{data.content}</p>
+                <h1 className="font-normal font-tggMedium text-5xl">
+                  {data.heading}
+                </h1>
+                <p className="text-gray-400 text-sm font-semibold font-tggBold mt-4">
+                  {data.content}
+                </p>
                 <CaseStudyLink href={data.fullCaseLink} />
               </div>
             );
